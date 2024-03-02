eaa5d215e281c1f68bac6c82f08f390582b0c6fc
23vbq <42277522+23vbq@users.noreply.github.com>
2024-03-02 17:13:29 +0100

    ConfigLoader class header

---
+++ server/include/configloader.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_LOADER_H
+#define CONFIG_LOADER_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class ConfigLoader{
+    private:
+        string m_path;
+        unordered_map<string, string> m_properties;
+    public:
+        ConfigLoader();
+        ConfigLoader(string);
+        string GetPath();
+        void Load(); // musi rzucać exepszyn
+        void Load(string); // może się tego pozbyć i filenae jako const?
+        template <typename T>
+        bool GetProperty(string key, T& valueOut);
+}
+#endif
