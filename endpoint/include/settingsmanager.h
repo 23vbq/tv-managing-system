8eb819cb9f874b0a77adfb754ff238415dadd5e2
23vbq <42277522+23vbq@users.noreply.github.com>
2024-05-06 23:38:41 +0200

    [E] SettingsManager: Header

---
+++ endpoint/include/settingsmanager.h
@@ -0,0 +1,18 @@
+#ifndef SETTINGS_MANAGER_H
+#define SETTINGS_MANAGER_H
+
+#include <syslog.h>
+
+#include "endpointsettings.h"
+
+class SettingsManager{
+private:
+    EndpointSettings m_settings;
+public:
+    SettingsManager();
+    ~SettingsManager();
+
+    void SetSettings();
+};
+
+#endif
