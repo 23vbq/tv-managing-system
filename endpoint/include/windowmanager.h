38b0ca9ae39560df2fc55d22cb34c55c1fb15514
23vbq <42277522+23vbq@users.noreply.github.com>
2024-05-14 21:41:39 +0200

    [E] WindowManager: Small improvements

--- endpoint/include/windowmanager.h
+++ endpoint/include/windowmanager.h
@@ -1,11 +1,11 @@
 #ifndef WINDOW_MANAGER_H
 #define WINDOW_MANAGER_H
 
-#define WM_POSX 500
-#define WM_POSY 500
-#define WM_WIDTH 500
-#define WM_HEIGHT 500
-#define WM_BORDER 15
+#define WM_POSX 0
+#define WM_POSY 0
+/*#define WM_WIDTH 500
+#define WM_HEIGHT 500*/
+#define WM_BORDER 0
 
 #include <X11/Xlib.h>
 #include <stdio.h>
@@ -20,6 +20,9 @@ private:
     Window m_wnd;
 
     XEvent m_event;
+
+    unsigned int m_width;
+    unsigned int m_height;
 public:
     WindowManager();
     ~WindowManager();
--- endpoint/src/windowmanager.cpp
+++ endpoint/src/windowmanager.cpp
@@ -3,21 +3,28 @@
 // Constructor
 
 WindowManager::WindowManager(){
+    // Open display
     m_display = XOpenDisplay(NULL);
     if (m_display == NULL)
         throw "Cannot open display";
+    // Get default screen and root window
     m_src = DefaultScreen(m_display);
     m_rootWnd = RootWindow(m_display, m_src);
+    // Initialize width, height
+    m_width = WidthOfScreen(ScreenOfDisplay(m_display, m_src));
+    m_height = HeightOfScreen(ScreenOfDisplay(m_display, m_src));
 }
 WindowManager::~WindowManager(){
-
+    XUnmapWindow(m_display, m_wnd);
+    XDestroyWindow(m_display, m_wnd);
+    XCloseDisplay(m_display);
 }
 
 // Public functions
 
 void WindowManager::CreateWindow(){
     m_wnd = XCreateSimpleWindow(m_display, m_rootWnd,
-        WM_POSX, WM_POSY, WM_WIDTH, WM_HEIGHT, WM_BORDER,
+        WM_POSX, WM_POSY, m_width, m_height, WM_BORDER,
         BlackPixel(m_display, m_src), WhitePixel(m_display, m_src)
     );
     XMapWindow(m_display, m_wnd);
